9fb4f4363e58079f327927d1a6e473f171416a17
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-04-19 22:21:08 +0200

    Create instalacion.sh

---
+++ instalacion.sh
@@ -0,0 +1,8 @@
+echo "Bienvenido al asistente de instalacion de Ping.py, vamos a empezar por lo basico, vamos a instalar todas las librerias de las que depende este script"
+pip install os argparse sys socket struct select time signal
+chmod +x ./ping.py
+echo "Ya hemos acabado, ahora solo tiene que hacer en su terminal: sudo ./ping.py"
+echo "[!!!][+]TIENES QUE CONVERTIRTE EN ROOT O PONER EL SUDO"
+echo "Ahora se eliminara el asistente de instalacion de Ping+, Gracias por descargarlo :) ahora a disfrutar"
+echo "¡IMPORTANTE! Recuerda ser bueno, tu eres responsable de tus actos. Aunque con este script no puedas hacer nada, puedes utilizarlo junto con otros scripts"
+rm instalacion.sh
